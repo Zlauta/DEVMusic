a42bae136cdc8f3bb98944a6eb0833879eac63a6
Matias Eliseo Soria Indarte <matiassoriaindarte@gmail.com>
2025-08-27 20:35:32 -0300

    fixed arregle app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Error404 from "./Error404";
+import RegisterPage from "./pages/RegisterPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* el * significa “cualquier otra ruta” */}
+        <Route path="/registro" element={<RegisterPage></RegisterPage>} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
