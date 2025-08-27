7e7bd4c8e1abf88891bb9d3a35770d2b3aaa0616
Zlauta <Zlauuta23@gmail.com>
2025-08-27 19:56:21 -0300

    cree pagina register y realice las funciones para validar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import FormRegister from "./components/auth/FormRegister";
+import RegisterPage from "./pages/RegisterPage";
+import "sweetalert2/dist/sweetalert2.min.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <RegisterPage></RegisterPage>
+    </div>
+  );
 };
 
 export default App;
