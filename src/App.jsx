9f518fb594828048eb29a415a4b7f73a82f70dc2
Zlauta <Zlauuta23@gmail.com>
2025-09-02 12:21:42 -0300

    se modificaron funciones para hacerlas globales para registro, login y cambiamos alertas

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Error404 from "./Error404";
 import RegisterPage from "./pages/RegisterPage";
 import AdminPage from "./pages/AdminPage";
 import "sweetalert2/dist/sweetalert2.min.css";
+import LoginPage from "./pages/LoginPage";
 const App = () => {
   return (
     <Routes>
       {/* el * significa “cualquier otra ruta” */}
       <Route path="/registro" element={<RegisterPage></RegisterPage>} />
-
+      <Route path="/login" element={<LoginPage></LoginPage>}></Route>
       <Route path="/admin" element={<AdminPage></AdminPage>}></Route>
       <Route path="*" element={<Error404 />} />
     </Routes>
