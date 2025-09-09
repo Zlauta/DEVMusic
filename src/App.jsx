d1cfcdc22d645b4c9ea40c0ce36d98b9def5c3af
Zlauta <Zlauuta23@gmail.com>
2025-09-08 21:30:03 -0300

    fixapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 import "sweetalert2/dist/sweetalert2.min.css";
 import AppRouter from "./router/AppRouter";
 
 const App = () => {
   return <AppRouter />;
-
 };
-//export default App;
+export default App;
